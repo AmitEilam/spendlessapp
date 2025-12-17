52fe878c32a8f71d61ce6619fca441e0ccd93077
AmitEilam <amiteilam10@gmail.com>
2025-12-17 18:20:31 +0200

    Update Logo component to support dark mode with dynamic logo source
    - Modified the Logo component to conditionally render the logo image based on the current theme (dark or light mode).
    - Integrated useTheme hook to access darkMode state, enhancing visual consistency across themes.

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,12 +1,17 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useTheme } from './ThemeProvider';
 
 function Logo() {
+  const { darkMode } = useTheme();
+
   return (
     <Link href='/' className='flex items-center gap-4 z-10'>
       <Image
         className='w-10 h-10 sm:w-12 sm:h-12'
-        src='/logo.png'
+        src={darkMode ? '/logo-dark.jpg' : '/logo.png'}
         height={60}
         width={60}
         alt='logo'
